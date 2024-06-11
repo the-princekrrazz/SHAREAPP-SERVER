84df497df186ad0f3163c8f5eb7505fc55201933
The prince <116440263+the-princekrrazz@users.noreply.github.com>
2024-06-11 11:26:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/routes.js';
 import DBConnection from './database/db.js';
+import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
